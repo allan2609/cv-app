ab117d9ca58056cd764b269b65db24b75524b612
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2025-06-16 19:28:29 +0300

    Add styling for SkillsDisplay component

--- src/components/SkillsDisplay.jsx
+++ src/components/SkillsDisplay.jsx
@@ -1,12 +1,18 @@
+import "./SkillsDisplay.css"
+
 export default function SkillsDisplay ({ skillList }) {
   return (
-    <>
+    <div className="skills-display-section">
       <h2>Skills</h2>
-      <ul>
-        {skillList.map((skill, index) => (
-          <li key={index}>{skill}</li>
-        ))}
-      </ul>
-    </>
+      {skillList.length > 0 ? (
+        <ul>
+          {skillList.map((skill, index) => (
+            <li key={index}>{skill}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No skills added yet.</p>
+      )}
+    </div>
   );
 }
