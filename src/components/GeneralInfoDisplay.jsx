80ea2f887ce87bc02c6e67992f0e32740315af1d
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2025-07-05 11:39:21 +0300

    Add styling for GeneralInfoDisplay

--- src/components/GeneralInfoDisplay.jsx
+++ src/components/GeneralInfoDisplay.jsx
@@ -1,11 +1,13 @@
+import "./GeneralInfoDisplay.css";
+
 export default function GeneralInfoDisplay({ generalInfo }) {
   return (
-    <>
+    <div className="general-display-section">
       <h2>{generalInfo.firstName} {generalInfo.lastName}</h2>
       <p>{generalInfo.aboutMe}</p>
-      <p>Email: {generalInfo.email}</p>
-      <p>Phone: {generalInfo.phone}</p>
-      <p>Website: {generalInfo.website}</p>
-    </>
+      <p><span className="label">Email:</span> {generalInfo.email}</p>
+      <p><span className="label">Phone:</span> {generalInfo.phone}</p>
+      <p><span className="label">Website:</span> {generalInfo.website}</p>
+    </div>
   );
 }
