eca564cdbdb69a5b2f26699506284db2e9f0da36
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2025-07-06 18:29:42 +0300

    Add styling for professional experience form

--- src/components/ProfessionalExperienceForm.jsx
+++ src/components/ProfessionalExperienceForm.jsx
@@ -1,3 +1,5 @@
+import "./ProfessionalExperienceForm.css";
+
 export default function ProfessionalExperienceForm({ professionalExperience, setProfessionalExperience, setWorkList }) {
 
   function handleChange(e) {
@@ -30,37 +32,35 @@ export default function ProfessionalExperienceForm({ professionalExperience, set
   }
 
   return (
-    <>
+    <form onSubmit={handleSubmit} className="work-form-section">
       <h2>Professional experience</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="employer" >Employer</label>
-          <input name="employer" value={professionalExperience.employer} onChange={handleChange} type="text" id="employer" required></input>
-        </div>
-        <div>
-          <label htmlFor="position" >Position</label>
-          <input name="position" value={professionalExperience.position} onChange={handleChange} type="text" id="position" required></input>
-        </div>
-        <div>
-          <label htmlFor="beginYear" >Begin year</label>
-          <input name="beginYear" value={professionalExperience.beginYear} onChange={handleChange} type="number" id="beginYear" required></input>
-        </div>
-        <div>
-          <label htmlFor="endYear">End year</label>
-          <input name="endYear" value={professionalExperience.endYear} onChange={handleChange} type="number" id="endYear" disabled={professionalExperience.isCurrentlyEmployed}></input>
-          <div>
+      <div>
+        <label htmlFor="employer">Employer</label>
+        <input name="employer" value={professionalExperience.employer} onChange={handleChange} type="text" id="employer" required />
+      </div>
+      <div>
+        <label htmlFor="position">Position</label>
+        <input name="position" value={professionalExperience.position} onChange={handleChange} type="text" id="position" required />
+      </div>
+      <div>
+        <label htmlFor="beginYear">Begin year</label>
+        <input name="beginYear" value={professionalExperience.beginYear} onChange={handleChange} type="number" id="beginYear" required />
+      </div>
+      <div>
+        <label htmlFor="endYear">End year</label>
+        <input name="endYear" value={professionalExperience.endYear} onChange={handleChange} type="number" id="endYear" disabled={professionalExperience.isCurrentlyEmployed} />
+        <div className="checkbox-group">
           <input
-              type="checkbox"
-              id="isCurrentlyEmployed"
-              name="isCurrentlyEmployed"
-              checked={professionalExperience.isCurrentlyEmployed}
-              onChange={handleCheckboxChange}
-            />
-            <label htmlFor="isCurrentlyEmployed">Currently employed</label>
-          </div>
+            type="checkbox"
+            id="isCurrentlyEmployed"
+            name="isCurrentlyEmployed"
+            checked={professionalExperience.isCurrentlyEmployed}
+            onChange={handleCheckboxChange}
+          />
+          <label htmlFor="isCurrentlyEmployed">Currently employed</label>
         </div>
-        <button type="submit">Add</button>
-      </form>
-    </>
-  )
+      </div>
+      <button type="submit">Add</button>
+    </form>
+  );
 }
